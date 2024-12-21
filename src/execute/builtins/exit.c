193a598b07365132f2bd23736ea96bb1e44326c5
Plernta Sukjarernchaikul <plesukja@42bangkok.com>
2024-12-21 19:09:49 +0700

    move ft_is_num_arg up

--- src/execute/builtins/exit.c
+++ src/execute/builtins/exit.c
@@ -1,16 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   _exit.c                                            :+:      :+:    :+:   */
+/*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plesukja <plesukja@42bangkok.com>          +#+  +:+       +#+        */
+/*   By: plesukja <plesukja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 00:17:53 by plesukja          #+#    #+#             */
-/*   Updated: 2024/12/14 16:31:37 by plesukja         ###   ########.fr       */
+/*   Updated: 2024/12/21 19:08:12 by plesukja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+
+static bool ft_is_num_arg(char *str)
+{
+	int i;
+
+	if (str[0] == '-' || str[0] == '+')
+	{
+		if (!str[1])
+			return (false);
+	}
+	i = 1;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
 /*
 Common exit statuses:
 0: Success.
@@ -43,23 +63,4 @@ void	run_builtin_exit(t_shell *shell, char **args)
 		shell->exit_status = ft_atoi(args[1]) % 256;
 	free_array(args); //free_array
 	clean_and_exit(shell); //clean_and_exit
-}
-
-static bool ft_is_num_arg(char *str)
-{
-	int i;
-
-	if (str[0] == '-' || str[0] == '+')
-	{
-		if (!str[1])
-			return (false);
-	}
-	i = 1;
-	while (str[i])
-	{
-		if (!ft_isdigit(str[i]))
-			return (false);
-		i++;
-	}
-	return (true);
 }
