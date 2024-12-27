140e27c0c4e92904795b80de27b65773a6e9ae1b
Plernta Sukjarernchaikul <plesukja@42bangkok.com>
2024-12-27 13:59:50 +0700

    add free_array()

--- src/clean_and_exit.c
+++ src/clean_and_exit.c
@@ -1,6 +1,19 @@
 
 #include "minishell.h"
 
+void	free_array(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
 void	free_tree(t_token *token)
 {
 	t_redir	*redir;
