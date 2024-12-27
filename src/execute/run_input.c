14ef2aaa5f6533bbbac3bda16e1666e9cc9b99f7
Plernta Sukjarernchaikul <plesukja@42bangkok.com>
2024-12-27 16:00:43 +0700

    //parse_argument(), run_redirections(), run_pipe()

--- src/execute/run_input.c
+++ src/execute/run_input.c
@@ -3,99 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   run_input.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plesukja <plesukja@42bangkok.com>          +#+  +:+       +#+        */
+/*   By: plesukja <plesukja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 14:49:23 by plesukja          #+#    #+#             */
-/*   Updated: 2024/11/30 14:59:15 by plesukja         ###   ########.fr       */
+/*   Updated: 2024/12/27 16:00:35 by plesukja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// // non-builtin command have to be fork before executing
-// static void	run_non_builtin(char **new_args, t_shell *shell)
-// {
-// 	pid_t	pid;
-// 	int		status;
+// non-builtin command have to be fork before executing
+static void	run_non_builtin(char **new_args, t_shell *shell)
+{
+	pid_t	pid;
+	int		status;
 
-// 	pid = fork();
-// 	if (pid < 0)
-// 		error_exit("fork failed", shell);
-// 	else if (pid == 0)
-// 		execute(new_args, shell);
-// 	else
-// 	{
-// 		waitpid(pid, &status, 0);
-// 		if (WIFEXITED(status))
-// 			shell->exit_status = WEXITSTATUS(status);
-// 	}
-// }
+	pid = fork();
+	if (pid < 0)
+		error_exit("fork failed", shell);
+	else if (pid == 0)
+		execute(new_args, shell);
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			shell->exit_status = WEXITSTATUS(status);
+	}
+}
 
-// static void	run_non_piped_command(t_cmd *command, t_shell *shell)
-// {
-// 	char	**new_args;
+static void	run_non_piped_command(t_cmd *command, t_shell *shell)
+{
+	char	**new_args;
 
-// 	if (!command->av[0])
-// 		return ;
-// 	new_args = parse_arguments(command->av, shell);
-// 	if (!new_args)
-// 		error_exit("parse arguments failed", shell);
-// 	if (is_builtin_cd(new_args[0]))
-// 		run_builtin_cd(new_args, shell);
-// 	else
-// 		run_non_builtin(new_args, shell);
-// 	free_array(new_args);
-// }
+	if (!command->av[0])
+		return ;
+	new_args = parse_arguments(command->av, shell);
+	if (!new_args)
+		error_exit("parse arguments failed", shell);
+	if (is_builtin_cd(new_args[0]))
+		run_builtin_cd(new_args, shell);
+	else
+		run_non_builtin(new_args, shell);
+	free_array(new_args);
+}
 
-// static void	run_child_process(t_cmd *command, t_shell *shell)
-// {
-// 	char	**new_args;
+static void	run_child_process(t_cmd *command, t_shell *shell)
+{
+	char	**new_args;
 
-// 	if (!command->av[0])
-// 		return ;
-// 	new_args = parse_arguments(command->av, shell);
-// 	if (!new_args)
-// 		error_exit("parse arguments failed", shell);
-// 	if (is_builtin_cd(new_args[0]))
-// 		run_builtin_cd(new_args, shell);
-// 	else
-// 		execute(new_args, shell);
-// 	free_array(new_args);
-// }
+	if (!command->av[0])
+		return ;
+	new_args = parse_arguments(command->av, shell);
+	if (!new_args)
+		error_exit("parse arguments failed", shell);
+	if (is_builtin_cd(new_args[0]))
+		run_builtin_cd(new_args, shell);
+	else
+		execute(new_args, shell);
+	free_array(new_args);
+}
 
-// void	run_command(t_token *token, t_shell *shell)
-// {
-// 	if (!token)
-// 		exit(EXIT_FAILURE);
-// 	if (token->type == COMMAND)
-// 	{
-// 		if (shell->has_pipe)
-// 			run_child_process((t_cmd *)token, shell);
-// 		else
-// 			run_non_piped_command((t_cmd *)token, shell);
-// 	}
-// 	else if (token->type == REDIR)
-// 	{
-// 		run_redirections((t_redir *)token, shell);
-// 	}
-// 	else if (token->type == PIPE)
-// 	{
-// 		run_pipe((t_pipe *)token, shell);
-// 	}
-// }
+void	run_command(t_token *token, t_shell *shell)
+{
+	if (!token)
+		exit(EXIT_FAILURE);
+	if (token->type == COMMAND)
+	{
+		if (shell->has_pipe)
+			run_child_process((t_cmd *)token, shell);
+		else
+			run_non_piped_command((t_cmd *)token, shell);
+	}
+	else if (token->type == REDIR)
+	{
+		run_redirections((t_redir *)token, shell);
+	}
+	else if (token->type == PIPE)
+	{
+		run_pipe((t_pipe *)token, shell);
+	}
+}
 
 
-// void	run_input(t_token *token, t_shell *shell)
-// {
-// 	if (!token)
-// 		exit ();
-// 	if (token->type == COMMAND)
-// 		run_command((t_cmd *)token, shell);
-// 	if (token->type == REDIR)
-// 		run_redir((t_redir *)token, shell);
-// 	if (token->type == PIPE)
-// 		run_pipe((t_pipe *)token, shell);
-// }
+void	run_input(t_token *token, t_shell *shell)
+{
+	if (!token)
+		exit ();
+	if (token->type == COMMAND)
+		run_command((t_cmd *)token, shell);
+	if (token->type == REDIR)
+		run_redir((t_redir *)token, shell);
+	if (token->type == PIPE)
+		run_pipe((t_pipe *)token, shell);
+}
 
 // void	run_command(t_cmd *cmd, t_shell *shell)
 // {
@@ -119,17 +119,17 @@
 // 	free_array(new_args);
 // }
 
-// parse_arguments()
-// {
+parse_arguments()
+{
 
-// }
+}
 
-// run_redir()
-// {
+run_redir()
+{
 
-// }
+}
 
-// run_pipe()
-// {
+run_pipe()
+{
 	
-// }
+}
