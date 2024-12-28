260c83208d46c29168b610e79fe61b21770366c9
Plernta S <hg@Plerntas-MacBook-Pro.local>
2024-12-28 20:34:25 +0700

    move free_ fn to free.c

--- src/clean_and_exit.c
+++ src/clean_and_exit.c
@@ -14,50 +14,6 @@ void	free_array(char **arr)
 	free(arr);
 }
 
-void	free_env(t_env **env)
-{
-	t_env	*temp;
-
-	if (!env)
-		return ;
-	while (*env)
-	{
-		temp = *env;
-		*env = (*env)->next;
-		free(temp->key);
-		free(temp->value);
-		free(temp);
-	}
-	*env = NULL;
-}
-
-void	free_tree(t_token *token)
-{
-	t_redir	*redir;
-
-	if (!token)
-		return ;
-	if (token->type == COMMAND)
-		free((t_cmd *)token);
-	else if (token->type == REDIR)
-	{
-		redir = (t_redir *)token;
-		if (redir->mode == 'h' && redir->temp_file)
-		{
-			free(redir->temp_file);
-			redir->temp_file = NULL;
-		}
-		free_tree(redir->prior_token);
-		free(redir);
-	}
-	else if (token->type == PIPE)
-	{
-		free_tree(((t_pipe *)token)->left);
-		free_tree(((t_pipe *)token)->right);
-		free((t_pipe *)token);
-	}
-}
-
 void	restore_fd(t_shell *shell)
 {
 	if (shell->default_stdin != STDIN_FILENO)
