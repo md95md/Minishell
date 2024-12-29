cc529aaec8580136a44a3e6e08d213d770a21657
Plernta S <hg@Plerntas-MacBook-Pro.local>
2024-12-30 00:49:38 +0700

    add prefix static to fn

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -6,29 +6,52 @@
 /*   By: plesukja <plesukja@42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 13:30:06 by plesukja          #+#    #+#             */
-/*   Updated: 2024/12/30 00:17:53 by plesukja         ###   ########.fr       */
+/*   Updated: 2024/12/30 00:48:55 by plesukja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void	execute_error(char **args, char *s, char *message, t_shell *shell)
+void	execute(char **args, t_shell *shell)
 {
-	ft_putstr_fd("minishell: ", STDERR_FILENO);
-	if (s)
-		ft_putstr_fd(s, STDERR_FILENO);
-	ft_putstr_fd(message, STDERR_FILENO);
-	free_array(args);
-	clean_and_exit(shell);
+	char	*file_path;
+
+	if (!args || !args[0])
+	{
+		free_array(args);
+		clean_and_exit(shell);
+	}
+	if (ft_strchr(args[0], '/'))
+	{
+		check_path_executable(args, shell);
+		file_path = ft_strdup(args[0]);
+	}
+	else
+		file_path = get_file_path(args, shell);
+	if (!file_path)
+	{
+		shell->exit_status = 127;
+		execute_error(args, args[0], ": command not found\n", shell);
+	}
+	if (execve(file_path, args, shell->env) < 0)
+	{
+		free(file_path);
+		shell->exit_status = errno;
+		execute_error((args, NULL, "execute error\n", shell););
+	}
 }
 
-static void	check_access(char **args, t_shell *shell)
+//127: Command not found.
+//126: Command is found but not executable (permission denied, a directory).
+//stat() checks info about a file or directory. 0: success, -1: error.
+//access checks permissions for a file. 0: success, -1: failure
+static void	check_path_executable(char **args, t_shell *shell)
 {
-	char		*pathname;
-	struct stat	statbuf;
+	char			*path;
+	struct	stat	statbuf;
 
-	pathname = args[0];
-	if (stat(pathname, &statbuf) != 0)
+	path = args[0];
+	if (stat(path, &statbuf) < 0)
 	{
 		shell->exit_status = 127;
 		execute_error(args, pathname, ": No such file or directory\n", shell);
@@ -38,157 +61,14 @@ static void	check_access(char **args, t_shell *shell)
 		shell->exit_status = 126;
 		execute_error(args, pathname, ": Is a directory\n", shell);
 	}
-	if (access(pathname, X_OK) != 0)
+	if (access(path, X_OK) < 0)
 	{
 		shell->exit_status = 126;
 		execute_error(args, pathname, ": Permission denied\n", shell);
 	}
 }
 
-static char	**get_path_arr(t_env *env)
-{
-	char	**path_arr;
-	char	*path_value;
-
-	path_value = ft_getenv(env, "PATH");
-	if (!path_value)
-		return (NULL);
-	path_arr = ft_split(path_value, ':');
-	if (!path_arr)
-		return (NULL);
-	return (path_arr);
-}
-
-static char	*get_pathname(char **cmd, t_shell *shell)
-{
-	char	**path_arr;
-	char	*pathname;
-	char	*temp;
-	int		i;
-
-	path_arr = get_path_arr(shell->env);
-	if (!path_arr)
-		return (NULL);
-	i = 0;
-	while (path_arr[i])
-	{
-		temp = ft_strjoin(path_arr[i], "/");
-		pathname = ft_strjoin(temp, cmd[0]);
-		free(temp);
-		if (access(pathname, X_OK) == 0)
-		{
-			free_array(path_arr);
-			return (pathname);
-		}
-		free(pathname);
-		i++;
-	}
-	free_array(path_arr);
-	return (NULL);
-}
-
-void	execute(char **args, t_shell *shell)
-{
-	char	*pathname;
-
-	if (!args || !args[0])
-	{
-		free_array(args);
-		clean_and_exit(shell);
-	}
-	if (ft_strchr(args[0], '/'))
-	{
-		check_access(args, shell);
-		pathname = ft_strdup(args[0]);
-	}
-	else
-		pathname = get_pathname(args, shell);
-	if (!pathname)
-	{
-		shell->exit_status = 127;
-		execute_error(args, args[0], ": command not found\n", shell);
-	}
-	if (execve(pathname, args, shell->env_arr) < 0)
-	{
-		free(pathname);
-		shell->exit_status = errno;
-		execute_error(args, NULL, "execute error\n", shell);
-	}
-}
-
-// void	execute(char **args, t_shell *shell)
-// {
-// 	char	*file_path;
-
-// 	if (!args || !args[0])
-// 	{
-// 		free_array();
-// 		clean_and_exit(); //*****clean_and_exit******/
-// 	}
-// 	if (ft_strchr(args[0], '/'))
-// 	{
-// 		check_path_executable(args, shell);
-// 		file_path = ft_strdup(args[0]);
-// 	}
-// 	else
-// 		file_path = get_file_path(args, shell);
-// 	if (!file_path)
-// 	{
-// 		shell->exit_status = 127;
-// 		execute_error();
-// 	}
-// 	if (execve(file_path, args, shell->env) < 0)
-// 	{
-// 		free(file_path);
-// 		shell->exit_status = errno;
-// 		execute_error();
-// 	}
-// }
-
-void	fork_and_execute(char **new_args, t_shell *shell)
-{
-	pid_t	pid;
-	int		statu
-	pid = fork();
-	if (pid < 0)
-		error_exit("fork failed", shell);
-	else if (pid == 0)
-		execute(new_args, shell);
-	else
-	{
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status))
-			shell->exit_status = WEXITSTATUS(status);
-	}
-}
-//127: Command not found.
-//126: Command is found but not executable (permission denied, a directory).
-//stat() checks info about a file or directory. 0: success, -1: error.
-//access checks permissions for a file. 0: success, -1: failure
-void	check_path_executable(char **args, t_shell *shell)
-{
-	char			*path;
-	struct	stat	statbuf;
-
-	path = args[0];
-	if (stat(path, &statbuf) < 0)
-	{
-		shell->exit_status = 127;
-		execute_error();
-	}
-	if (S_ISDIR(statbuf.st_mode))
-	{
-		shell->exit_status = 126;
-		execute_error();
-	}
-	if (access(path, X_OK) < 0)
-	{
-		shell->exit_status = 126;
-		execute_error();
-	}
-}
-
-char	*get_file_path(char **args, t_shell *shell)
+static char	*get_file_path(char **args, t_shell *shell)
 {
 	char	*file_path;
 	char	*tmp;
@@ -216,7 +96,7 @@ char	*get_file_path(char **args, t_shell *shell)
 	return (NULL);
 }
 
-char	**get_path_arr(t_env *env)
+static char	**get_path_arr(t_env *env)
 {
 	char	**path_arr;
 	char	*path_value;
@@ -228,6 +108,16 @@ char	**get_path_arr(t_env *env)
 	if (!path_arr)
 		return (NULL);
 	return (path_arr);
+}
+
+static void	execute_error(char **args, char *s, char *message, t_shell *shell)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	if (s)
+		ft_putstr_fd(s, STDERR_FILENO);
+	ft_putstr_fd(message, STDERR_FILENO);
+	free_array(args);
+	clean_and_exit(shell);
 }
 
 // //path_arr 22/46
