036fa426339c2b15d8189c0c3e10e5874f59fb35
Plernta Sukjarernchaikul <plesukja@42bangkok.com>
2024-12-30 14:58:11 +0700

    move unset_env_arr() up

--- src/execute/builtins/unset.c
+++ src/execute/builtins/unset.c
@@ -6,27 +6,13 @@
 /*   By: plesukja <plesukja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 13:09:50 by plesukja          #+#    #+#             */
-/*   Updated: 2024/12/27 16:42:05 by plesukja         ###   ########.fr       */
+/*   Updated: 2024/12/30 14:56:55 by plesukja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void	run_builtin_unset(t_shell *shell, char **args)
-{
-	int	i;
-
-	i = 1;
-	while (args[i])
-	{
-		unset_env_arr(&shell->env, args[i]);
-		i++;
-	}
-	shell->env_arr = env_to_arr(shell->env, shell->env_arr);
-	shell->exit_status = EXIT_SUCCESS;
-}
-
-void	unset_env_arr(t_env **env, char *var)
+static void	unset_env_arr(t_env **env, char *var)
 {
 	t_env	*prev;
 	t_env	*curr;
@@ -48,4 +34,18 @@ void	unset_env_arr(t_env **env, char *var)
 		prev = curr;
 		curr = curr->next;
 	}
+}
+
+void	run_builtin_unset(t_shell *shell, char **args)
+{
+	int	i;
+
+	i = 1;
+	while (args[i])
+	{
+		unset_env_arr(&shell->env, args[i]);
+		i++;
+	}
+	shell->env_arr = env_to_arr(shell->env, shell->env_arr);
+	shell->exit_status = EXIT_SUCCESS;
 }
