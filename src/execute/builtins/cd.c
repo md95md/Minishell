e4c89ba1f5cb1f4a0bed46571f8a38b16b212971
Plernta Sukjarernchaikul <plesukja@42bangkok.com>
2024-12-30 15:01:17 +0700

    move run_builtin_cd() last

--- src/execute/builtins/cd.c
+++ src/execute/builtins/cd.c
@@ -6,11 +6,76 @@
 /*   By: plesukja <plesukja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 00:17:49 by plesukja          #+#    #+#             */
-/*   Updated: 2024/12/27 16:23:28 by plesukja         ###   ########.fr       */
+/*   Updated: 2024/12/30 15:00:15 by plesukja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+static bool	has_too_many_arguments(char **args)
+{
+	return (args[1] && args[2]);
+}
+
+//getenv returns a pointer to the value in the env
+//getenv returns NULL if there is no match
+// chdir: success, zero is returned
+// chdir: error, -1 is returned
+static int	change_directory(t_shell *shell, char **args, char *old_pwd)
+{
+	char	*home_dir;
+
+	if (!args[1])
+	{
+		home_dir = getenv("HOME");
+		if (!home_dir || chdir(home_dir) == -1)
+		{
+			handle_cd_error(shell, NULL, old_pwd);
+			return (EXIT_FAILURE);
+		}
+	}
+	else if (chdir(args[1]) == -1)
+	{
+		handle_cd_error(shell, NULL, old_pwd);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+static void	update_path_var(t_env *env, char *old_pwd, char *new_pwd)
+{
+	char	*node_old_pwd;
+	char	*node_new_pwd;
+
+	if (!old_pwd || !new_pwd)
+		return ;
+	node_old_pwd = ft_strjoin("OLDPWD=", old_pwd);
+	node_new_pwd = ft_strjoin("PWD=", new_pwd);
+	if (!node_old_pwd || !node_new_pwd)
+	{
+		free(node_old_pwd);
+		free(node_new_pwd);
+		return ;
+	}
+	add_or_update_env_var(&env, node_old_pwd);
+	add_or_update_env_var(&env, node_new_pwd);
+	free(node_old_pwd);
+	free(node_new_pwd);
+}
+
+static void	handle_cd_error(t_shell *shell, char *msg, char *old_pwd)
+{
+	if (msg)
+	{
+		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+		ft_putendl_fd(msg, STDERR_FILENO);
+	}
+	else
+		perror("minishell: cd");
+	if (old_pwd)
+		free (old_pwd);
+	shell->exit_status = EXIT_FAILURE;
+}
 
 void	run_builtin_cd(t_shell *shell, char **args)
 {
@@ -35,69 +100,4 @@ void	run_builtin_cd(t_shell *shell, char **args)
 	free(new_pwd);
 	shell->env_arr = env_to_arr(shell->env, shell->env_arr);
 	shell->exit_status = EXIT_SUCCESS;
-}
-
-bool	has_too_many_arguments(char **args)
-{
-	return (args[1] && args[2]);
-}
-
-//getenv returns a pointer to the value in the env
-//getenv returns NULL if there is no match
-// chdir: success, zero is returned
-// chdir: error, -1 is returned
-int	change_directory(t_shell *shell, char **args, char *old_pwd)
-{
-	char	*home_dir;
-
-	if (!args[1])
-	{
-		home_dir = getenv("HOME");
-		if (!home_dir || chdir(home_dir) == -1)
-		{
-			handle_cd_error(shell, NULL, old_pwd);
-			return (EXIT_FAILURE);
-		}
-	}
-	else if (chdir(args[1]) == -1)
-	{
-		handle_cd_error(shell, NULL, old_pwd);
-		return (EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
-}
-
-void	update_path_var(t_env *env, char *old_pwd, char *new_pwd)
-{
-	char	*node_old_pwd;
-	char	*node_new_pwd;
-
-	if (!old_pwd || !new_pwd)
-		return ;
-	node_old_pwd = ft_strjoin("OLDPWD=", old_pwd);
-	node_new_pwd = ft_strjoin("PWD=", new_pwd);
-	if (!node_old_pwd || !node_new_pwd)
-	{
-		free(node_old_pwd);
-		free(node_new_pwd);
-		return ;
-	}
-	add_or_update_env_var(&env, node_old_pwd);
-	add_or_update_env_var(&env, node_new_pwd);
-	free(node_old_pwd);
-	free(node_new_pwd);
-}
-
-void	handle_cd_error(t_shell *shell, char *msg, char *old_pwd)
-{
-	if (msg)
-	{
-		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-		ft_putendl_fd(msg, STDERR_FILENO);
-	}
-	else
-		perror("minishell: cd");
-	if (old_pwd)
-		free (old_pwd);
-	shell->exit_status = EXIT_FAILURE;
 }
