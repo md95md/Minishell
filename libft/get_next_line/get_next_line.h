8d532ffdec016b7adfdedc4d890c961787042966
Plernta S <hg@Plerntas-MacBook-Pro.local>
2024-12-31 13:35:45 +0700

    edited

--- libft/get_next_line/get_next_line.h
+++ libft/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: plesukja <plesukja@42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 00:47:21 by plesukja          #+#    #+#             */
-/*   Updated: 2024/09/13 15:49:41 by plesukja         ###   ########.fr       */
+/*   Updated: 2024/12/31 13:35:34 by plesukja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@
 # define BUFFER_SIZE 2
 # endif
 
-void	*ft_calloc(size_t size, size_t count);
-int		ft_strlen(char *s);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strchr(const char *s, int c);
+void	*ft_gnl_calloc(size_t size, size_t count);
+int		ft_gnl_strlen(char *s);
+char	*ft_gnl_strjoin(const char *s1, const char *s2);
+char	*ft_gnl_strchr(const char *s, int c);
 char	*get_next_line(int fd);
 
 #endif
