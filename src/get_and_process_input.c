e352bb1df2cef95debbb6ed7999c5c9e7e4e7c41
Plernta Sukjarernchaikul <plesukja@42bangkok.com>
2025-01-02 13:30:58 +0700

    edited

---
+++ src/get_and_process_input.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_and_process_input.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plesukja <plesukja@42bangkok.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 12:35:44 by plesukja          #+#    #+#             */
+/*   Updated: 2025/01/01 18:32:38 by plesukja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	*init_prompt(t_shell *shell)
+{
+	char	*pwd;
+	char	*user;
+	char	*prompt;
+
+	pwd = ft_getenv(shell->env, "PWD");
+	user = ft_getenv(shell->env, "USER");
+	printf("init_prompt: pwd = %s\n", pwd);
+	printf("init_prompt: user = %s\n", user);
+	prompt = NULL;
+	if (!pwd || !user)
+		return (ft_strdup("$ "));
+	return (prompt);
+}
+
+int		get_input(char **line, t_shell *shell)
+{
+	char	*prompt;
+
+	prompt = init_prompt(shell);
+	printf("get_input: after init_prompt: prompt = %s\n", prompt);
+	run_signals(1, shell);
+	printf("get_input: after run_signals()\n");
+	*line = readline(prompt);
+	printf("get_input: after readline()\n");
+	free(prompt);
+	if (!*line)
+		return (-1);
+	if (*line && **line)
+		add_history(*line);
+	return (0);
+}
+
+void	process_input(t_shell *shell, char *input)
+{
+	if (!build_tree(shell, input))
+		return ;
+	run_signals(2, shell);
+	run_input(shell->current_cmd, shell);
+}
+
+bool	build_tree(t_shell *shell, char *input)
+{
+	shell->current_cmd = process_token(input);
+	if (!shell->current_cmd)
+	{
+		shell->exit_status = 2;
+		free(input);
+		return (false);
+	}
+	else if (shell->current_cmd->type == COMMAND \
+		&& !((t_cmd *)(shell->current_cmd))->av[0])
+	{
+		free_tree(shell->current_cmd);
+		free(input);
+		return (false);
+	}
+	if (shell->current_cmd->type == PIPE)
+		shell->has_pipe = 1;
+	shell->default_stdin = dup(STDIN_FILENO);
+	shell->default_stdout = dup(STDOUT_FILENO);
+	return (true);
+}
