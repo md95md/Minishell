d7c4d72d025e56f646bbd5373fb32660b8f09c2d
Plernta Sukjarernchaikul <plesukja@42bangkok.com>
2025-01-02 14:42:26 +0700

    edited

--- src/get_and_process_input.c
+++ src/get_and_process_input.c
@@ -6,7 +6,7 @@
 /*   By: plesukja <plesukja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 12:35:44 by plesukja          #+#    #+#             */
-/*   Updated: 2025/01/02 13:38:40 by plesukja         ###   ########.fr       */
+/*   Updated: 2025/01/02 14:42:06 by plesukja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ static char	*init_prompt(t_shell *shell)
 
 	pwd = ft_getenv(shell->env, "PWD");
 	user = ft_getenv(shell->env, "USER");
-	printf("init_prompt: pwd = %s\n", pwd);
-	printf("init_prompt: user = %s\n", user);
 	prompt = NULL;
 	if (!pwd || !user)
 		return (ft_strdup("$ "));
@@ -36,11 +34,8 @@ int	get_input(char **line, t_shell *shell)
 	char	*prompt;
 
 	prompt = init_prompt(shell);
-	printf("get_input: after init_prompt: prompt = %s\n", prompt);
 	run_signals(1, shell);
-	printf("get_input: after run_signals()\n");
 	*line = readline(prompt);
-	printf("get_input: after readline()\n");
 	free(prompt);
 	if (!*line)
 		return (-1);
@@ -51,6 +46,7 @@ int	get_input(char **line, t_shell *shell)
 
 void	process_input(t_shell *shell, char *input)
 {
+	printf("process_input\n");
 	if (!build_tree(shell, input))
 		return ;
 	run_signals(2, shell);
@@ -59,6 +55,7 @@ void	process_input(t_shell *shell, char *input)
 
 bool	build_tree(t_shell *shell, char *input)
 {
+	printf("build_tree\n");
 	shell->current_cmd = process_token(input);
 	if (!shell->current_cmd)
 	{
