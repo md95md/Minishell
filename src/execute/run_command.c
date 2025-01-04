c1c093d14da37570b503c9249fab2775d49bc125
Plernta S <hg@Plerntas-MacBook-Pro.local>
2025-01-04 13:36:38 +0700

    edited

--- src/execute/run_command.c
+++ src/execute/run_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   run_command.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plesukja <plesukja@student.42.fr>          +#+  +:+       +#+        */
+/*   By: plesukja <plesukja@42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 10:46:41 by plesukja          #+#    #+#             */
-/*   Updated: 2024/12/30 18:32:57 by plesukja         ###   ########.fr       */
+/*   Updated: 2025/01/04 13:36:17 by plesukja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,22 @@ static char	**parse_arguments(char **args, t_shell *shell)
 	int		i;
 	int		j;
 
+	printf ("parse_arguments\n");
 	i = 0;
 	j = 0;
 	new_args = ft_calloc(array_len(args) + 1, sizeof(char *));
 	while (args[i])
 	{
+		printf (".parse_arguments\n");
 		tmp = unquote_and_expand_var(args[i], shell);
+		printf ("..parse_arguments\n");
 		if (tmp[0])
 			new_args[j++] = tmp;
 		else
 			free(tmp);
 		i++;
 	}
+	printf ("parse_arguments: finished\n");
 	return (new_args);
 }
 
@@ -38,6 +42,7 @@ void	run_command(t_cmd *cmd, t_shell *shell)
 {
 	char	**new_args;
 
+	printf ("run_command: %s\n", cmd->av[0]);
 	if (!cmd->av[0])
 		return ;
 	new_args = parse_arguments(cmd->av, shell);
@@ -52,6 +57,7 @@ void	run_command(t_cmd *cmd, t_shell *shell)
 		else
 			fork_and_execute(new_args, shell);
 	}
+	printf ("...run_command\n");
 	free_array(new_args);
 }
 
@@ -60,6 +66,7 @@ void	fork_and_execute(char **new_args, t_shell *shell)
 	pid_t	pid;
 	int		status;
 
+	printf ("fork_and_execute\n");
 	pid = fork();
 	if (pid < 0)
 		error_exit("fork failed", shell);
