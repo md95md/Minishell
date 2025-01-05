81bf024daed679f81025851a0345a0466e89c694
Plernta Sukjarernchaikul <plesukja@42bangkok.com>
2025-01-05 18:22:36 +0700

    edited

--- src/execute/unquote_and_expand_var2.c
+++ src/execute/unquote_and_expand_var2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unquote_and_expand_var2.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plesukja <plesukja@42bangkok.com>          +#+  +:+       +#+        */
+/*   By: plesukja <plesukja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 01:03:37 by plesukja          #+#    #+#             */
-/*   Updated: 2025/01/04 13:57:47 by plesukja         ###   ########.fr       */
+/*   Updated: 2025/01/05 18:21:55 by plesukja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,27 +34,41 @@ void	single_quote_handler(char **s, char **result)
 
 void	double_quote_handler(char **s, char **result, t_shell *shell)
 {
-	int		i;
-	char	*temp;
+	// int		i;//
+	// char	*temp;//
 
-	i = 0;
+	// i = 0;//
 	*s += 1;
 	if (is_empty(s))
 		return ;
-	while ((*s)[i] && (*s)[i] != '"')
+
+	// **s != 0 && **s != '"'//
+	// while ((*s)[i] && (*s)[i] != '"')//
+	while(**s != 0 && **s != '"')
 	{
-		if ((*s)[i] == '$')
-			double_quote_dollar_handler(s, result, shell);
+		if (**s == '\'')
+			single_quote_handler(s, result);
+		// else if (*s == '"')//
+		// 	double_quote_handler(&s, &result, shell);//
+		else if (**s == '$')
+			dollar_sign_handler(s, result, shell);
 		else
-		{
-			while ((*s)[i] && (*s)[i] != '$' && (*s)[i] != '"')
-				i++;
-			temp = ft_substr(&(*s)[0], 0, i);
-			*result = ft_strjoin_free(*result, temp);
-			*s += i;
-		}
+			character_handler(s, result);
 	}
-	*s += 1;
+
+		// **s == '$'//
+	// 	if ((*s)[i] == '$')//
+	// 		double_quote_dollar_handler(s, result, shell);//
+	// 	else//
+	// 	{//
+	// 		while ((*s)[i] && (*s)[i] != '$' && (*s)[i] != '"')//
+	// 			i++;//
+	// 		temp = ft_substr(&(*s)[0], 0, i);//
+	// 		*result = ft_strjoin_free(*result, temp);//
+	// 		*s += i;//
+	// 	}//
+	// }//
+	// *s += 1;//
 }
 
 void	double_quote_dollar_handler(char **s, char **result, t_shell *shell)
@@ -69,15 +83,17 @@ void	dollar_sign_handler(char **s, char **result, t_shell *shell)
 {
 	char	*temp;
 
-	if (*s[1] == '\'')
+	*s += 1;
+	if (**s == '\'')
 		single_quote_handler(s, result);
-	else if (*s[1] == '"')
+	else if (**s == '"')
 		double_quote_handler(s, result, shell);
 	else
 	{
 		temp = var_expansion(s, shell);
 		*result = ft_strjoin_free(*result, temp);
 	}
+	printf("dollar_sign_handler: **s = %i\n", **s);
 }
 
 void	character_handler(char **s, char **result)
